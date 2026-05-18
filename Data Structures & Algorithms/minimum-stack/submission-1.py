c42ecc475523d0bd8b8a3a02592abdd8f9855c37
DhanushR <146897841+Dhanush-67@users.noreply.github.com>
2026-05-17 23:37:04 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.arr = []
+        self.min_arr = []
+        
+
+    def push(self, val: int) -> None:
+        self.arr.append(val)
+
+        if not self.min_arr:
+            self.min_arr.append(val)
+        else:
+            self.min_arr.append(min(self.min_arr[-1], val))
+
+    def pop(self) -> None:
+        self.arr.pop()
+        self.min_arr.pop()
+        
+
+    def top(self) -> int:
+        return self.arr[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min_arr[-1]
+        
+        
